a702d3cc5d4b339a916edcdb78e06c63b1281801
Yan Komizorov <yanko20@gmail.com>
2016-03-21 16:26:57 -0400

    Assignment 34 - Data Source Pattern: Completed

--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,6 +2,7 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import io.bloc.android.blocly.BloclyApplication;
@@ -12,11 +13,15 @@ import io.bloc.android.blocly.R;
  */
 public class BloclyActivity extends Activity{
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
         Toast.makeText(this, BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
         Toast.LENGTH_LONG).show();
+        textView = (TextView)findViewById(R.id.mainTextView);
+        textView.setText(BloclyApplication.getSharedDataSource().getFeeds().get(0).getDescription());
     }
 }
