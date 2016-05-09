9deea682a5bf1012699b281eabcc0bb37e89edb0
Yan Komizorov <yanko20@gmail.com>
2016-05-09 15:13:57 -0400

    Checkpoint 51 Databases Part 1: completed

---
+++ app/src/main/java/io/bloc/android/blocly/api/model/database/table/Table.java
@@ -0,0 +1,18 @@
+package io.bloc.android.blocly.api.model.database.table;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by yankomizorov on 5/9/16.
+ */
+public abstract class Table {
+
+    protected static final String COLUMN_ID = "id";
+
+    public abstract String getName();
+    public abstract String getCreateStatement();
+
+    public void onUpgrade(SQLiteDatabase writableDatabase, int oldVersion, int newVersion){
+
+    }
+}
