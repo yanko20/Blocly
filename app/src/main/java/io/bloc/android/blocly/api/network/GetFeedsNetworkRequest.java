c3dd97174244f75253a63ea9590d1747c31b5827
Yan Komizorov <yanko20@gmail.com>
2016-05-09 01:36:57 -0400

    Checkpoint 50 Download Feeds Part 2: completed

--- app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
+++ app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
@@ -1,16 +1,36 @@
 package io.bloc.android.blocly.api.network;
 
-import android.util.Log;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * Created by yankomizorov on 5/4/16.
  */
-public class GetFeedsNetworkRequest extends NetworkRequest {
+public class GetFeedsNetworkRequest extends NetworkRequest<List<GetFeedsNetworkRequest.FeedResponse>> {
+
+
+    public static final int ERROR_PARSING = 3;
+    private static final String XML_TAG_TITLE = "title";
+    private static final String XML_TAG_DESCRIPTION = "description";
+    private static final String XML_TAG_LINK = "link";
+    private static final String XML_TAG_ITEM = "item";
+    private static final String XML_TAG_PUB_DATE = "pubDate";
+    private static final String XML_TAG_GUID = "guid";
+    private static final String XML_TAG_ENCLOSURE = "enclosure";
+    private static final String XML_ATTRIBUTE_URL = "url";
+    private static final String XML_ATTRIBUTE_TYPE = "type";
 
     String[] feedUrls;
 
@@ -19,26 +39,121 @@ public class GetFeedsNetworkRequest extends NetworkRequest {
     }
 
     @Override
-    public Object performRequest() {
+    public List<FeedResponse> performRequest() {
+        List<FeedResponse> responseFeeds = new ArrayList<>(feedUrls.length);
         for(String feedUrlString : feedUrls){
             InputStream inputStream = openStream(feedUrlString);
             if(inputStream == null){
                 return null;
             }
             try{
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line = bufferedReader.readLine();
-                while(line != null){
-                    Log.v(getClass().getSimpleName(), "Line: " + line);
-                    line = bufferedReader.readLine();
+                DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                Document xmlDocument = documentBuilder.parse(inputStream);
+                String channelTitle = optFirstTagFromDocument(xmlDocument, XML_TAG_TITLE);
+                String channelDescription = optFirstTagFromDocument(xmlDocument, XML_TAG_DESCRIPTION);
+                String channelURL = optFirstTagFromDocument(xmlDocument, XML_TAG_LINK);
+
+                NodeList allItemNodes = xmlDocument.getElementsByTagName(XML_TAG_ITEM);
+                List<ItemResponse> responseItems = new ArrayList<>(allItemNodes.getLength());
+                for(int itemIndex = 0; itemIndex < allItemNodes.getLength(); itemIndex++){
+                    String itemURL = null;
+                    String itemTitle = null;
+                    String itemDescription = null;
+                    String itemGUID = null;
+                    String itemPubDate = null;
+                    String itemEnclosureURL = null;
+                    String itemEnclosureMIMEType = null;
+
+                    Node itemNode = allItemNodes.item(itemIndex);
+                    NodeList tagNodes = itemNode.getChildNodes();
+                    for(int tagIndex = 0; tagIndex < tagNodes.getLength(); tagIndex++){
+                        Node tagNode = tagNodes.item(tagIndex);
+                        String tag = tagNode.getNodeName();
+
+                        if(XML_TAG_LINK.equalsIgnoreCase(tag)){
+                            itemURL = tagNode.getTextContent();
+                        }else if (XML_TAG_TITLE.equalsIgnoreCase(tag)) {
+                            itemTitle = tagNode.getTextContent();
+                        } else if (XML_TAG_DESCRIPTION.equalsIgnoreCase(tag)) {
+                            itemDescription = tagNode.getTextContent();
+                        } else if (XML_TAG_ENCLOSURE.equalsIgnoreCase(tag)) {
+                            NamedNodeMap enclosureAttributes = tagNode.getAttributes();
+                            itemEnclosureURL = enclosureAttributes.getNamedItem(XML_ATTRIBUTE_URL).getTextContent();
+                            itemEnclosureMIMEType = enclosureAttributes.getNamedItem(XML_ATTRIBUTE_TYPE).getTextContent();
+                        }else if (XML_TAG_PUB_DATE.equalsIgnoreCase(tag)) {
+                            itemPubDate = tagNode.getTextContent();
+                        } else if (XML_TAG_GUID.equalsIgnoreCase(tag)) {
+                            itemGUID = tagNode.getTextContent();
+                        }
+                    }
+                    responseItems.add(new ItemResponse(itemURL, itemTitle, itemDescription,
+                            itemGUID, itemPubDate, itemEnclosureURL, itemEnclosureMIMEType));
                 }
-                bufferedReader.close();
+                responseFeeds.add(new FeedResponse(feedUrlString, channelTitle, channelURL, channelDescription, responseItems));
             }catch(IOException e){
                 e.printStackTrace();
                 setErrorCode(ERROR_IO);
                 return null;
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+                setErrorCode(ERROR_PARSING);
+                return null;
+            } catch (SAXException e) {
+                e.printStackTrace();
+                setErrorCode(ERROR_PARSING);
+                return null;
             }
+        }
+        return responseFeeds;
+    }
+
+    private String optFirstTagFromDocument(Document document, String tagName){
+        NodeList elementsByTag = document.getElementsByTagName(tagName);
+        if(elementsByTag.getLength() > 0){
+            return elementsByTag.item(0).getTextContent();
         }
         return null;
     }
+
+
+    public static class FeedResponse{
+        public final String channelFeedUrl;
+        public final String channelTitle;
+        public final String channelURL;
+        public final String channelDescription;
+        public final List<ItemResponse> channelItems;
+
+
+        FeedResponse(String channelFeedUrl, String channelTitle, String channelURL,
+        String channelDescription, List<ItemResponse> channelItems){
+            this.channelFeedUrl = channelFeedUrl;
+            this.channelTitle = channelTitle;
+            this.channelURL = channelURL;
+            this.channelDescription = channelDescription;
+            this.channelItems = channelItems;
+        }
+    }
+
+    public static class ItemResponse{
+        public final String itemURL;
+        public final String itemTitle;
+        public final String itemDescription;
+        public final String itemGUID;
+        public final String itemPubDate;
+        public final String itemEnclosureURL;
+        public final String itemEnclosureMIMEType;
+
+        ItemResponse(String itemURL, String itemTitle, String itemDescription,
+                     String itemGUID, String itemPubDate, String itemEnclosureURL,
+                     String itemEnclosureMIMEType) {
+            this.itemURL = itemURL;
+            this.itemTitle = itemTitle;
+            this.itemDescription = itemDescription;
+            this.itemGUID = itemGUID;
+            this.itemPubDate = itemPubDate;
+            this.itemEnclosureURL = itemEnclosureURL;
+            this.itemEnclosureMIMEType = itemEnclosureMIMEType;
+        }
+    }
+
 }
