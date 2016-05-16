5cada18bbffbf29f957a0034a993839f2c417d10
Yan Komizorov <yanko20@gmail.com>
2016-05-15 21:33:06 -0400

    Checkpoint 53 Databases Part 3: completed

--- app/src/main/java/io/bloc/android/blocly/api/model/database/table/RssFeedTable.java
+++ app/src/main/java/io/bloc/android/blocly/api/model/database/table/RssFeedTable.java
@@ -1,10 +1,44 @@
 package io.bloc.android.blocly.api.model.database.table;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by yankomizorov on 5/9/16.
  */
 public class RssFeedTable extends Table{
 
+    public static class Builder implements Table.Builder{
+
+        ContentValues values = new ContentValues();
+
+        public Builder setSiteURL(String siteURL){
+            values.put(COLUMN_LINK, siteURL);
+            return this;
+        }
+
+        public Builder setFeedURL(String feedURL){
+            values.put(COLUMN_FEED_URL, feedURL);
+            return this;
+        }
+
+        public Builder setTitle(String title){
+            values.put(COLUMN_TITLE, title);
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            values.put(COLUMN_DESCRIPTION, description);
+            return this;
+        }
+
+        @Override
+        public long insert(SQLiteDatabase writabeDB) {
+            return writabeDB.insert(NAME, null, values);
+        }
+    }
+
+    private static final String NAME = "rss_feeds";
     private static final String COLUMN_LINK = "link";
     private static final String COLUMN_TITLE = "title";
     private static final String COLUMN_DESCRIPTION = "description";
--- app/src/main/java/io/bloc/android/blocly/api/model/database/table/RssItemTable.java
+++ app/src/main/java/io/bloc/android/blocly/api/model/database/table/RssItemTable.java
@@ -1,9 +1,62 @@
 package io.bloc.android.blocly.api.model.database.table;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by yankomizorov on 5/9/16.
  */
 public class RssItemTable extends Table {
+
+    public static class Builder implements Table.Builder{
+
+        ContentValues values = new ContentValues();
+
+        public Builder setLink(String link){
+            values.put(COLUMN_LINK, link);
+            return this;
+        }
+
+        public Builder setTitle(String title){
+            values.put(COLUMN_TITLE, title);
+            return this;
+        }
+
+        public Builder setDescription(String description){
+            values.put(COLUMN_DESCRIPTION, description);
+            return this;
+        }
+
+        public Builder setGUID(String guid) {
+            values.put(COLUMN_GUID, guid);
+            return this;
+        }
+
+        public Builder setPubDate(long pubDate) {
+            values.put(COLUMN_PUB_DATE, pubDate);
+            return this;
+        }
+
+        public Builder setEnclosure(String enclosure) {
+            values.put(COLUMN_ENCLOSURE, enclosure);
+            return this;
+        }
+
+        public Builder setMIMEType(String mimeType) {
+            values.put(COLUMN_MIME_TYPE, mimeType);
+            return this;
+        }
+
+        public Builder setRSSFeed(long rssFeed) {
+            values.put(COLUMN_RSS_FEED, rssFeed);
+            return this;
+        }
+
+        @Override
+        public long insert(SQLiteDatabase writabeDB) {
+            return writabeDB.insert(NAME, null, values);
+        }
+    }
 
     private static final String COLUMN_LINK = "link";
     private static final String COLUMN_TITLE = "title";
@@ -15,6 +68,7 @@ public class RssItemTable extends Table {
     private static final String COLUMN_RSS_FEED = "rss_feed";
     private static final String COLUMN_FAVORITE = "is_favorite";
     private static final String COLUMN_ARCHIVED = "is_archived";
+    private static final String NAME = "rss_items";
 
     @Override
     public String getName() {
